01a46008a4c29412460e23fc453b424b7b65f699
uasi <uasi@99cm.org>
2009-06-18 14:45:50 +0900

    Extract a link post with href for anchored text

--- TKGenericExtractor.m
+++ TKGenericExtractor.m
@@ -65,10 +65,24 @@
 - (TKPost *)postFromSource:(TKSource *)source
 {
     TKPost *post = [[TKPost alloc] initWithType:TKPostLinkType];
-    [post setTitle:[source title]];
-    [post setBody:[source text]];
-    [post setURL:[source URL]];
-    [post setLinkURL:[source linkURL]];
+    
+    /* This behavior may be confusing... */
+    
+    if ([source linkURL] != nil) {
+        if ([source text] != nil && ! [[source text] isEqualToString:@""]) {
+            [post setTitle:[source text]];
+        }
+        else {
+            [post setTitle:[[source linkURL] absoluteString]];
+        }
+        [post setURL:[source linkURL]];
+        /* Let's not setBody: */
+    }
+    else {
+        [post setTitle:[source title]];
+        [post setURL:[source URL]];
+        [post setBody:[source text]];
+    }
     return [post autorelease];
 }
 
